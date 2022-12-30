d84faa798f9f397fe268db992300a6defe21ae68
Mrgil <dieoxin4@naver.com>
2022-12-30 11:47:30 +0900

    메인페이지 추가

---
+++ routes/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+let fs = require('fs');
+const {Op} = require("sequelize");
+const jwt = require("jsonwebtoken");
+const {Server} = require("http"); // 1. 모듈 불러오기
+// const authMiddleware = require("./middlewares/auth-middleware");
+const user = require("./users.routes")
+const path = require("path");
+myPath = path.join("static", "view");
+
+const app = express();
+
+const http = Server(app); // 2. express app을 http 서버로 감싸기
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.createReadStream(myPath+"/home.html").pipe(res);
+});
+
+app.use("/users", [user]);
+
+module.exports = http;
---
+++ routes/users.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// const PostsController = require('../controllers/posts.controller');
+// const postsController = new PostsController();
+
+// router.get('/', postsController.getPosts);
+// router.post('/', postsController.createPost);
+// router.get('/:postId', postsController.getPostbyId);
+// router.put('/:postId', postsController.modifyPost);
+
+module.exports = router;
