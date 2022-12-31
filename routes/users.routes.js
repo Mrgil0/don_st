1af62d101112f74ce72f222949827d882b6678dc
Mrgil <dieoxin4@naver.com>
2023-01-01 02:35:03 +0900

    Update users.routes.js

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,13 +4,6 @@ const router = express.Router();
 // const PostsController = require('../controllers/posts.controller');
 // const postsController = new PostsController();
 
-const PostsController = require('../controllers/posts.controller');
-const postsController = new PostsController();
-
-// const PostsController = require('../controllers/posts.controller');
-// const postsController = new PostsController();
-
-
 // router.get('/', postsController.getPosts);
 // router.post('/', postsController.createPost);
 // router.get('/:postId', postsController.getPostbyId);
